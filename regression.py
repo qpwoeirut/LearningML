bc04174161b9ab9a90fdcf6fc8eed44db784ef0f
qpwoeirut <stanleyzhong8@gmail.com>
2022-04-19 15:02:00 -0700

    Did a second tutorial about regression

---
+++ regression.py
@@ -0,0 +1,90 @@
+# https://www.tensorflow.org/tutorials/keras/regression
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+
+# Make NumPy printouts easier to read.
+np.set_printoptions(precision=3, suppress=True)
+
+import tensorflow as tf
+
+from tensorflow import keras
+from tensorflow.keras import layers
+
+url = 'http://archive.ics.uci.edu/ml/machine-learning-databases/auto-mpg/auto-mpg.data'
+column_names = ['MPG', 'Cylinders', 'Displacement', 'Horsepower', 'Weight',
+                'Acceleration', 'Model Year', 'Origin']
+
+raw_dataset = pd.read_csv(url, names=column_names,
+                          na_values='?', comment='\t',
+                          sep=' ', skipinitialspace=True)
+
+dataset = raw_dataset.copy()
+dataset = dataset.dropna()
+dataset['Origin'] = dataset['Origin'].map({1: 'USA', 2: 'Europe', 3: 'Japan'})
+dataset = pd.get_dummies(dataset, columns=['Origin'], prefix='', prefix_sep='')
+
+train_dataset = dataset.sample(frac=0.8, random_state=0)
+test_dataset = dataset.drop(train_dataset.index)
+
+# sns.pairplot(train_dataset[['MPG', 'Cylinders', 'Displacement', 'Weight']], diag_kind='kde')
+# plt.show()
+
+train_features = train_dataset.copy()
+test_features = test_dataset.copy()
+
+train_labels = train_features.pop('MPG')
+test_labels = test_features.pop('MPG')
+
+normalizer = tf.keras.layers.Normalization(axis=-1)
+normalizer.adapt(np.array(train_features))
+
+
+def build_and_compile_model(norm):
+    model = keras.Sequential([
+        norm,
+        layers.Dense(64, activation='relu'),
+        layers.Dense(64, activation='relu'),
+        layers.Dense(1)
+    ])
+
+    model.compile(loss='mean_absolute_error',
+                  optimizer=tf.keras.optimizers.Adam(0.001))
+    return model
+
+
+dnn_model = build_and_compile_model(normalizer)
+
+dnn_model.fit(
+    train_features,
+    train_labels,
+    validation_split=0.2,
+    verbose=0, epochs=100)
+
+test_results = {
+    "dnn_model": dnn_model.evaluate(test_features, test_labels, verbose=0)
+}
+print(pd.DataFrame(test_results, index=['Mean absolute error [MPG]']).T)
+
+test_predictions = dnn_model.predict(test_features).flatten()
+
+a = plt.axes(aspect='equal')
+plt.scatter(test_labels, test_predictions)
+plt.xlabel('True Values [MPG]')
+plt.ylabel('Predictions [MPG]')
+lims = [0, 50]
+plt.xlim(lims)
+plt.ylim(lims)
+plt.plot(lims, lims)
+plt.show()
+
+error = test_predictions - test_labels
+plt.hist(error, bins=25)
+plt.xlabel('Prediction Error [MPG]')
+plt.ylabel('Count')
+plt.show()
+
+dnn_model.save('regression_model')
+
