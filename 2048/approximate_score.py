36fc99fbf34654603ef900199f2a2c0f8dab380e
qpwoeirut <stanleyzhong8@gmail.com>
2022-04-22 10:15:41 -0700

    Add a few models

--- 2048/approximate_score.py
+++ 2048/approximate_score.py
@@ -9,25 +9,27 @@ import tensorflow as tf
 from tensorflow import keras
 from keras import layers
 
-BOARD_COUNT = 100000
-NEURONS = 64
-LAYERS = 6
-EPOCHS = 50
-LEARNING_RATE = 0.001
+# https://stats.stackexchange.com/questions/181/how-to-choose-the-number-of-hidden-layers-and-nodes-in-a-feedforward-neural-netw
+BOARD_COUNT = 500000
+NEURONS = 20
+LAYERS = 2
+EPOCHS = 500
+LEARNING_RATE = 0.005
 
 MODEL_IDENTIFIER = f"{LAYERS}-{NEURONS}-{EPOCHS}-{LEARNING_RATE}"
 
-pathlib.Path(MODEL_IDENTIFIER).mkdir(exist_ok=True)
-
 
 def generate_board() -> dict:
+    board = [random.randint(0, 13) for _ in range(16)]
     as_dict = dict()
     as_dict["score"] = 0
-    for r in range(4):
-        for c in range(4):
-            tile = random.randint(0, 16)
-            as_dict[f"{r},{c}"] = tile
-            as_dict["score"] += 0 if tile <= 1 else (tile - 1) * (1 << tile)
+    for pos in range(16):
+        as_dict[f"a_{pos}"] = 0 if board[pos] == 0 else 1 << board[pos]
+        as_dict["score"] += 0 if board[pos] <= 1 else (board[pos] - 1) * (1 << board[pos])
+        if pos != 3 and pos != 7 and pos != 11 and pos != 15:
+            as_dict[f"eq_{pos},{pos+1}"] = 1 if board[pos] == board[pos + 1] else 0
+        if pos < 12:
+            as_dict[f"eq_{pos},{pos+4}"] = 1 if board[pos] == board[pos + 4] else 0
     return as_dict
 
 
@@ -49,6 +51,7 @@ def save_actual_predicted_plot(test_predictions):
     plt.xlabel('Actual Score')
     plt.ylabel('Predicted Score')
     max_score = max(boards, key=lambda b: b["score"])["score"]
+    max_score = min(max_score, 8e7)
     lims = [0, max_score]
     plt.xlim(lims)
     plt.ylim(lims)
@@ -83,6 +86,7 @@ test_labels = test_features.pop('score')
 print("Separated dataset into training and testing datasets")
 
 model_layers = [layers.Dense(NEURONS, activation='relu') for _ in range(LAYERS)] + [layers.Dense(1)]
+stopper = tf.keras.callbacks.EarlyStopping(monitor='loss', patience=5, min_delta=10000)
 model = keras.Sequential(model_layers)
 model.compile(
     loss=tf.keras.losses.MeanSquaredError(),
@@ -92,20 +96,22 @@ history = model.fit(
     train_features,
     train_labels,
     validation_split=0.2,
-    epochs=EPOCHS)
+    epochs=EPOCHS,
+    callbacks=[stopper]
+)
 print("Model fitted")
-
-save_loss_plot(history)
 
 evaluation = model.evaluate(test_features, test_labels)
 print("Evaluation:", evaluation)
 
-test_predictions = model.predict(test_features).flatten()
-save_actual_predicted_plot(test_predictions)
-
-difference = test_predictions - test_labels
-save_difference_plot(difference)
-
 name = input('Hit enter to save or q to abort ').strip()
 if name != "q":
+    pathlib.Path(MODEL_IDENTIFIER).mkdir(exist_ok=True)
+    save_loss_plot(history)
+
+    test_predictions = model.predict(test_features).flatten()
+    save_actual_predicted_plot(test_predictions)
+
+    difference = test_predictions - test_labels
+    save_difference_plot(difference)
     model.save("model_" + MODEL_IDENTIFIER)
