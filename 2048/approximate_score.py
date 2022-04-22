f77ef340e44672291fd66e976ba5eabd3ee3f345
qpwoeirut <stanleyzhong8@gmail.com>
2022-04-22 09:16:36 -0700

    Still trying to train a good model

--- 2048/approximate_score.py
+++ 2048/approximate_score.py
@@ -1,19 +1,23 @@
+import pathlib
 import random
 
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-
-# Make NumPy printouts easier to read.
-np.set_printoptions(precision=3, suppress=True)
-
 import tensorflow as tf
 
 from tensorflow import keras
-from tensorflow.keras import layers
+from keras import layers
 
 BOARD_COUNT = 100000
 NEURONS = 64
+LAYERS = 6
+EPOCHS = 50
+LEARNING_RATE = 0.001
+
+MODEL_IDENTIFIER = f"{LAYERS}-{NEURONS}-{EPOCHS}-{LEARNING_RATE}"
+
+pathlib.Path(MODEL_IDENTIFIER).mkdir(exist_ok=True)
 
 
 def generate_board() -> dict:
@@ -25,6 +29,40 @@ def generate_board() -> dict:
             as_dict[f"{r},{c}"] = tile
             as_dict["score"] += 0 if tile <= 1 else (tile - 1) * (1 << tile)
     return as_dict
+
+
+def save_loss_plot(history):
+    plt.plot(history.history['loss'], label='loss')
+    plt.plot(history.history['val_loss'], label='val_loss')
+    plt.ylim([0, max(history.history["loss"])])
+    plt.xlabel('Epoch')
+    plt.ylabel('Error')
+    plt.legend()
+    plt.grid(True)
+    plt.savefig(pathlib.Path(MODEL_IDENTIFIER) / "loss")
+    plt.clf()
+
+
+def save_actual_predicted_plot(test_predictions):
+    plt.axes(aspect='equal')
+    plt.scatter(test_labels, test_predictions)
+    plt.xlabel('Actual Score')
+    plt.ylabel('Predicted Score')
+    max_score = max(boards, key=lambda b: b["score"])["score"]
+    lims = [0, max_score]
+    plt.xlim(lims)
+    plt.ylim(lims)
+    plt.plot(lims, lims)
+    plt.savefig(pathlib.Path(MODEL_IDENTIFIER) / "actual_predicted")
+    plt.clf()
+
+
+def save_difference_plot(differences):
+    plt.hist(differences, bins=25)
+    plt.xlabel('Prediction Difference')
+    plt.ylabel('Count')
+    plt.savefig(pathlib.Path(MODEL_IDENTIFIER) / "difference")
+    plt.clf()
 
 
 print("Starting board generation")
@@ -44,56 +82,30 @@ test_labels = test_features.pop('score')
 
 print("Separated dataset into training and testing datasets")
 
-model = keras.Sequential([
-    layers.Dense(NEURONS, activation='relu'),
-    layers.Dense(NEURONS, activation='relu'),
-    layers.Dense(NEURONS, activation='relu'),
-    layers.Dense(NEURONS, activation='relu'),
-    layers.Dense(NEURONS, activation='relu'),
-    layers.Dense(NEURONS, activation='relu'),
-    layers.Dense(NEURONS, activation='relu'),
-    layers.Dense(NEURONS, activation='relu'),
-    layers.Dense(1)
-])
+model_layers = [layers.Dense(NEURONS, activation='relu') for _ in range(LAYERS)] + [layers.Dense(1)]
+model = keras.Sequential(model_layers)
 model.compile(
     loss=tf.keras.losses.MeanSquaredError(),
-    optimizer=tf.keras.optimizers.Adam(0.001))
+    optimizer=tf.keras.optimizers.Adam(LEARNING_RATE))
 print("Model compiled")
-model.fit(
+history = model.fit(
     train_features,
     train_labels,
     validation_split=0.2,
-    epochs=100)
+    epochs=EPOCHS)
 print("Model fitted")
+
+save_loss_plot(history)
 
 evaluation = model.evaluate(test_features, test_labels)
 print("Evaluation:", evaluation)
 
 test_predictions = model.predict(test_features).flatten()
-
-plt.axes(aspect='equal')
-plt.scatter(test_labels, test_predictions)
-plt.xlabel('Actual Score')
-plt.ylabel('Predicted Score')
-max_score = max(boards, key=lambda b: b["score"])["score"]
-lims = [0, max_score]
-plt.xlim(lims)
-plt.ylim(lims)
-plt.plot(lims, lims)
-plt.show()
+save_actual_predicted_plot(test_predictions)
 
 difference = test_predictions - test_labels
-plt.hist(difference, bins=25)
-plt.xlabel('Prediction Difference')
-plt.ylabel('Count')
-plt.show()
+save_difference_plot(difference)
 
-quotient = test_predictions / test_labels
-plt.hist(quotient, bins=25)
-plt.xlabel('Prediction Quotient')
-plt.ylabel('Count')
-plt.show()
-
-name = input('Enter model name or "q" to abort.').strip()
+name = input('Hit enter to save or q to abort ').strip()
 if name != "q":
-    model.save(name)
+    model.save("model_" + MODEL_IDENTIFIER)
